3f15b6408a2906e2aaad7f08ab003821c9bc8708
Murilo <barranco@Murilos-MacBook-Pro.local>
2023-09-11 08:30:27 -0300

    feature first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head"
+
+import styles from "./home.module.scss"
+import { SubscribeButton } from "../components/SubscribeButton"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | IgNews</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🖐🏻 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
+    </>
+  )
+}
