63f1082d4b20db3fde40d0d619c1d37d1b7e0afd
MuriloBarranco <murilo_cesar7@hotmail.com>
2023-10-24 17:16:02 -0300

    new class

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -16,7 +16,7 @@ type User = {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const session = await getSession({ req })
+    const session = await getSession({ req })////////////
 
     const user = await fauna.query<User>(
       q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
