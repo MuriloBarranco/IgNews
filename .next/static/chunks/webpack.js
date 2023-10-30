f77f7f60b24aca0cdb62f12423f09a0c551a1138
MuriloBarranco <murilo_cesar7@hotmail.com>
2023-10-30 11:07:57 -0300

    feature: new class day

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a6daa15b92a64871"; }
+/******/ 		__webpack_require__.h = function() { return "c5981f313e1d4b69"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
